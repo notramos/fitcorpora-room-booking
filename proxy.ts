a9ed186b28817c8d6c760d4b644d33daac980e67
notramos <sergioguntur18@gmail.com>
2026-08-27 15:59:10 +0700

    refactor: rename middleware.ts to proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import type { NextRequest } from "next/server";
 
-export default async function middleware(request: NextRequest) {
+export default async function proxy(request: NextRequest) {
   const token = await getToken({
     req: request,
     secret: process.env.NEXTAUTH_SECRET,
